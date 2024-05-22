9b6492b76cb10b38576e2d8e21194e6969e68044
Alessandro Albanesi <alessandro.albanesi@studio.unibo.it>
2024-05-22 21:57:40 +0200

    Enable backup database in preprocessing service

--- data_preprocessing/fanta_nba_data_preprocessing_service/mongo_db_connector.py
+++ data_preprocessing/fanta_nba_data_preprocessing_service/mongo_db_connector.py
@@ -5,21 +5,52 @@ from urllib.parse import quote_plus
 class MongoDBConnector:
     def __init__(self, config_file):
         self.config = self._load_config(config_file)
+        self._client = None
+        self._db = None
+        self._connect_to_db()
 
     def _load_config(self, config_file):
         with open(config_file, 'r') as f:
             config = yaml.safe_load(f)
         return config
-
+    
+    def _is_connected(self):
+        print("Checking if connected to MongoDB...")
+        try:
+            connected = self._client is not None and self._client.is_primary
+            return connected
+        except Exception as e:
+            print("Error checking connection to MongoDB: ", e)
+            return False
+        
     def _connect_to_db(self):
-        uri = f"mongodb://{quote_plus(self.config['user'])}:{quote_plus(self.config['password'])}@{self.config['host']}"
-        print("Connection uri: ", uri)
-        client = pymongo.MongoClient(uri, port=self.config['port'])
-        return client[self.config['database']]
+        main_db_uri = f"mongodb://{quote_plus(self.config['user'])}:{quote_plus(self.config['password'])}@{self.config['host']}"
+        print("Connection uri (main database): ", main_db_uri)
+
+        self._client = pymongo.MongoClient(main_db_uri, port=self.config['port'], serverSelectionTimeoutMS=1000)
+        self._db = self._client[self.config['database']]
+
+        if self._is_connected():
+            print("Connected to MongoDB successfully!")
+            return
+        else:
+            print("Connecting to backup database")            
+            backup_db_uri = f"mongodb://{quote_plus(self.config['user'])}:{quote_plus(self.config['password'])}@{self.config['backup_host']}"
+            print("Connection uri (backup database): ", backup_db_uri)
+            self._client = pymongo.MongoClient(backup_db_uri, port=self.config['backup_port'], serverSelectionTimeoutMS=1000)
+            self._db = self._client[self.config['database']]
+            if self._is_connected():
+                print("Connected to MongoDB (backup) successfully!")
+                return
+            else:
+                print("Error connecting to MongoDB")
+                raise Exception("Error connecting to MongoDB")
+        
 
     def insert_data(self, data: dict):
-        client = self._connect_to_db()
-        collection = client[self.config["collection"]]
+        if not self._is_connected():
+            self._connect_to_db()
+        collection = self._db[self.config["collection"]]
         result = collection.insert_one(data)
         print("Data inserted with ID:", result.inserted_id)
 
@@ -32,8 +63,9 @@ class MongoDBConnector:
         return self.get_most_recent_doc(self.config['teams_stats_collection_name'])
 
     def get_most_recent_doc(self, collection_name):
-        client = self._connect_to_db()
-        collection = client[collection_name]
+        if not self._is_connected():
+            self._connect_to_db()
+        collection = self._db[collection_name]
         most_recent_doc = collection.find_one(sort=[("_id", pymongo.DESCENDING)])
         if not most_recent_doc:
             print(f"No documents found!")
@@ -41,4 +73,10 @@ class MongoDBConnector:
         print(f"Found {len(most_recent_doc)} records")
         most_recent_doc.pop("_id")
         key = list(most_recent_doc.keys())[0]
-        return most_recent_doc[key]    
+        return most_recent_doc[key]
+    
+
+if __name__=='__main__':
+    m = MongoDBConnector("config/default.yaml")
+    m.get_games_stats()
+    m.get_teams_stats()
